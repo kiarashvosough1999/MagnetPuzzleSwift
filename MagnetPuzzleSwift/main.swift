291b1108a4fe704c1a3c28637e03be5803ae8e83
kiarashvosough1999 <vosough.k@gmail.com>
2022-01-03 16:51:04 +0330

    files added

--- MagnetPuzzleSwift/main.swift
+++ MagnetPuzzleSwift/main.swift
@@ -7,8 +7,16 @@
 
 import Foundation
 
-let board = BoardMaker(inputName:"/Users/kiarashvosough/Desktop/Magnet/Magnet/input1_method2.txt")
-    .getBoard()
-let csp = CSP(board: board)
-csp.setup()
+let inputsPath = "/Users/kiarashvosough/Desktop/MagnetPuzzleSwift/MagnetPuzzleSwift/Inputs/"
+
+let input1 = "input1_method1.txt"
+let input2 = "input1_method2.txt"
+let input3 = "input1_method3.txt"
+
+for item in [input1, input2, input3] {
+    let board = BoardMaker(inputName: inputsPath + item).getBoard()
+    let csp = CSP(board: board)
+    csp.setup()
+    sleep(20)
+}
 
